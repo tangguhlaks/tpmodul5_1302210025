0e78aa7c6f64d67e5a01b60f84303c5568096689
tangguhlaks <tangguhlaksana0@gmail.com>
2023-03-19 21:30:00 +0700

    menambahkan class HaloGeneric dengan method

--- tpmod5_1302210025/tpmod5_1302210025/Program.cs
+++ tpmod5_1302210025/tpmod5_1302210025/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HelloGeneric helloGeneric = new HelloGeneric();
+        helloGeneric.SapaUser("Tangguh Laksana");
+    }
+}
+
+class HelloGeneric
+{
+    public void SapaUser(string username)
+    {
+        Console.WriteLine("Halo User " + username);
+    }
+}
